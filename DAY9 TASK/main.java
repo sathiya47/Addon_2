fa1623a0749abc3c73e666111370aafe7a7145f8
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 09:53:04 +0530

    day 9 completed

--- DAY9 TASK/main.java
+++ DAY9 TASK/main.java
@@ -1,5 +1,21 @@
 package DAY9;
 
 public class main {
+	public static void main(String[] args) {   //rank finder
+		
+        int[] arr = {10, 20, 20, 30, 40}; // sorted array
+        int target = 20;
+        int rank = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= target)
+                rank++;
+            else
+                break;
+        }
+
+        System.out.println("Rank of " + target + " is: " + rank);
+    }
 }
+
+
